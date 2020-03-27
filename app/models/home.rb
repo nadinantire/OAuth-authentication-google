393e5ec6aff9807b9ed88a1ff1c5373a9703eabf
mygithubangebenie <angebenie23@gmail.com>
2020-03-27 15:29:59 +0200

    testig

---
+++ app/models/home.rb
@@ -0,0 +1,16 @@
+class Home < ApplicationRecord
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contact Form Inquiry",
+      :to => "nadinantire@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
